d0ea8e07f3e4514a5f4977d899776d58d99af163
irasunat2 <ahmadhendi06@gmail.com>
2022-06-21 23:34:21 -0700

    class object

--- coba.java
+++ coba.java
@@ -1,14 +1,35 @@
-public class coba {
-    
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
+class dataktp{//class
+    String nama; //variabel
+    String tgl_lahir;
+    int nik;
+    String alamat;
+    String golongandarah;
 
-            for (int y = 0; y < i; y++) {
-                //inner loop
-                System.out.print(" * ");
+   dataktp (String inputnama, String inputtgl_lahir, int inputnik, String inputalamat, String inputgolongandarah){
 
-          }
-    }  
+   
+
+    nama =inputnama;
+    tgl_lahir =inputtgl_lahir;
+        nik =inputnik;
+    alamat =inputalamat;
+   golongandarah =inputgolongandarah;
+
+   System.out.println(nama);
+    System.out.println(tgl_lahir);
+    System.out.println(nik);
+    System.out.println(alamat);
+    System.out.println(golongandarah);
+
+   }
+}
+
+public class coba{
+    public static void main(String[] args){//method
+
+        dataktp dataktp1 = new dataktp("adi","12-26-1999",224562323,"jalan.dr sutomo","A"); //object
+        System.out.println(" ");
+
     }
+
 }
